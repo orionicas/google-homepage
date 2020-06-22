e2a4db42ebce84a73d6ef26b39cc00ab3f2592f3
Fiona Vuong <fvuong8@gmail.com>
2020-06-23 01:11:08 +0800

    add app icon

---
+++ scripts.js
@@ -0,0 +1,22 @@
+var x = 26
+var y = 65
+var t = "fiona stinks"
+var fdsfsdfdsf = "phum stinks"
+
+
+console.log(y + t)
+
+function poo(p){
+    var u = 5 + 7 + p
+    console.log(u)
+    return u
+}
+
+var result = poo(1)
+console.log(result)
+
+var googlelogo = document.querySelector("#google");
+googlelogo.addEventListener("click", () => {
+    googlelogo.style.size = "50px";
+});
+
